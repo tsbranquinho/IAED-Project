f8e586e3c0b6e184c5c85ac3ccba147fc084f73c
tbranquinho18 <tiagodsbranquinho@tecnico.ulisboa.pt>
2023-03-30 19:16:53 +0100

    constants v0.1

---
+++ constants.h
@@ -0,0 +1,47 @@
+#ifndef constants
+#define constants
+
+#define NO_ROUTE -1 /*just to check if there's a route                        */
+#define FALSE 0 /*boolean value: false                                        */
+#define EQUAL 0 /*when two strings are the same                               */
+#define TRUE 1 /*boolean value: true                                          */
+#define INV_LEN 3 /*length of the abbreviation inv                            */
+#define MAX_ARGUMENTS 5 /*maximum number of arguments from a command          */
+#define INVERSO_LEN 7 /*length of the word inverso                            */
+#define ROUTE_NAME_LENGTH 21 /*maximum length of a route's name               */
+#define STOP_NAME_LENGTH 51 /*maximum length of a stop's name                 */
+#define MAX_ROUTES 200 /*maximum number of routes                             */
+#define BUFSIZ 8192 /*maximum number of characters in a line                  */
+#define MAX_STOPS 10000 /*maximum number of stops                             */
+#define MAX_CONNECTIONS 30000 /*maximum number of connections                 */
+#define INVERSO "inverso" /*the word inverso*/
+
+typedef struct { /*struct of routes*/
+    char name[ROUTE_NAME_LENGTH];
+    char first_stop[STOP_NAME_LENGTH];
+    char last_stop[STOP_NAME_LENGTH];
+    int start_index;
+    int end_index;
+    double cost;
+    double duration;
+    int stops_number;
+} Route;
+
+typedef struct { /*struct of stops*/
+    char name[STOP_NAME_LENGTH];
+    double latitude;
+    double longitude;
+    int routes_passing;
+} Stop;
+
+typedef struct { /*struct of connections*/
+    char route_name[ROUTE_NAME_LENGTH];
+    char initial_stop[STOP_NAME_LENGTH];
+    char final_stop[STOP_NAME_LENGTH];
+    int next_index;
+    int prev_index;
+    double cost;
+    double duration;
+} Connection;
+
+#endif /*constants*/
