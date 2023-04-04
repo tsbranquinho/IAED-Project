4b799aa09929818203f91b7b7d1b6d40483b8630
tbranquinho18 <tiagodsbranquinho@tecnico.ulisboa.pt>
2023-04-04 23:33:25 +0100

    prototypes v0.2

--- prototypes.h
+++ prototypes.h
@@ -2,30 +2,31 @@
 #define prototypes
 #include "constants.h"
 
-int handle_command(char line[], char command, Stop stops[], Route routes[], Connection connections[]);
-int parser(char line[], char arguments[][STOP_NAME_LENGTH]);
-Stop add_routes_passing(Stop p_stop, Connection connections[], Route routes[]);
-void create_stop(char arguments[][STOP_NAME_LENGTH], Stop stops[]);
-void list_stops(Stop stops[], Connection connections[], Route routes[]);
-int check_stop(char arguments[], Stop stops[], int arg_number);
-void command_p(char line[], Stop stops[], Connection connections[], Route routes[]);
+int handle_command(char line[], char command, Stop stops[], Route routes[], Connection connections[], int *route_num, int *stop_num, int *connection_num);
+int parser(char line[], char ***arguments, int max_arguments);
+Stop add_routes_passing(Stop p_stop, Connection connection[], Route routes[], int route_num);
+void create_stop(char **arguments, Stop stops[], int *stop_num);
+void list_stops(Stop stops[], Connection connections[], Route routes[], int stop_num, int route_num);
+int check_stop(char arguments[], Stop stops[], int arg_number, int stop_num);
+void command_p(char line[], Stop stops[],Connection connections[], Route routes[], int *stop_num, int route_num);
 int is_inverted(char argument[]);
-void create_route(char arguments[][STOP_NAME_LENGTH], Route routes[]);
-int get_stops_route(char arguments[][STOP_NAME_LENGTH], Route routes[], Connection connections[]);
-void get_inverted_stops_route(char arguments[][STOP_NAME_LENGTH], Route routes[], Connection connections[]);
+void create_route(char **arguments, Route routes[], int *route_num);
+int get_stops_route(char **arguments, Route routes[], Connection connections[], int route_num);
+void get_inverted_stops_route(char **arguments, Route routes[], Connection connections[], int route_num);
 void print_route_description(Route spec_route);
-void command_c(char line[], Route routes[],  Connection connections[]);
-void create_connection(char arguments[][STOP_NAME_LENGTH], Route routes[], Connection connections[], int i);
-void add_route_information(char arguments[][STOP_NAME_LENGTH], Route routes[], int i);
-void change_route_information(char args[][STOP_NAME_LENGTH], Route routes[], Connection connections[], int i, int add_end);
-void command_l(char line[], Route routes[], Stop stops[], Connection connections[]);
-int find_route(char arguments[][STOP_NAME_LENGTH], Route routes[]);
-int find_stops(char arguments[][STOP_NAME_LENGTH], Stop stops[]);
-int is_valid_connection(char arguments[][STOP_NAME_LENGTH], Route routes[], Stop stops[]);
+void command_c(char line[], Route routes[],  Connection connections[], int *route_num);
+void create_connection(char **arguments, Route routes[], Connection connections[], int i, int connection_num);
+void add_route_information(char **arguments, Route routes[], int i, int connection_num);
+void change_route_information(char **args, Route routes[], Connection connections[], int i, int add_end, int connection_num);
+void command_l(char line[], Route routes[], Stop stops[], Connection connections[], int *connection_num, int route_num, int stop_num);
+int find_route(char **arguments, Route routes[], int route_num);
+int find_stops(char **arguments, Stop stops[], int stop_num);
+int is_valid_connection(char **arguments, Route routes[], Stop stops[], int stop_num, int route_num);
 int check_first_and_last(Stop spec_stop, Route spec_route, int j, int list_of_routes[], int pos);
 void print_routes_passing(int pos, int list_of_routes[], Stop spec_stop, Route routes[]);
-void command_i(Stop stops[], Route routes[], Connection connections[]);
+void command_i(Stop stops[], Route routes[], Connection connections[], int stop_num, int route_num);
 void bubble_sort(Route routes[], int list_of_routes[], int size);
+void free_arguments(char **arguments, int arg_number);
 
 
 #endif /*prototypes*/
