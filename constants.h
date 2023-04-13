2799c049478848b60a73e69ae39a81d7d52e0b38
tbranquinho18 <tiagodsbranquinho@tecnico.ulisboa.pt>
2023-04-13 21:23:14 +0100

    constants

--- constants.h
+++ constants.h
@@ -1,18 +1,16 @@
 #ifndef constants
 #define constants
 
-#define END -2
 #define NO_ROUTE -1 /*just to check if there's a route                        */
-#define ERROR -1 /*just to check if there's an error                          */
+#define ERROR -1 /*just to check if there's an error allocating memory        */
+#define NO_CONNECTIONS 0 /*just to check if there are connections in a route  */
 #define FALSE 0 /*boolean value: false                                        */
 #define EQUAL 0 /*when two strings are the same                               */
 #define TRUE 1 /*boolean value: true                                          */
 #define INV_LEN 3 /*length of the abbreviation inv                            */
 #define INVERSO_LEN 7 /*length of the word inverso                            */
-#define ROUTE_INCREMENT 100 /*increment of the allocs for routes              */
-#define STOP_INCREMENT 1000 /*increment of the allocs for stops               */
 #define BUFFER 65535 /*maximum number of characters in a line                 */
-#define INVERSO "inverso" /*the word inverso*/
+#define INVERSO "inverso" /*the word inverso                                  */
 
 typedef struct { /*struct of connections*/
     char *route_name;
@@ -22,7 +20,7 @@ typedef struct { /*struct of connections*/
     double duration;
 } Connection;
 
-typedef struct Linked {
+typedef struct Linked { /*struct of linked list for connections*/
     Connection spec_connection;
     struct Linked *next;
     struct Linked *prev;
